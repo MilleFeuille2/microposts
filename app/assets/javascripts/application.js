85bd157578e8baea7e0c5477c66fc51340397374
yasu_take <ysdhrtk.227@gmail.com>
2015-12-06 11:37:44 +0000

    temp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require i18n
+//= require i18n/translations
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  before_filter :set_locale
+  def set_locale
+    I18n.locale = extract_locale_from_subdomai
+  end
+  
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : I18n.default_locale
+  end
+  
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
   private
   def logged_in_user
     unless logged_in?
